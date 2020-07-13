6dd2005f1a38e6488f21db78f061ca8c06e7f69e
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 08:33:08 -0300

    set recycler view and progress bar

--- app/src/main/java/com/desafio/githubsearchjavapop/controller/MainActivity.java
+++ app/src/main/java/com/desafio/githubsearchjavapop/controller/MainActivity.java
@@ -1,10 +1,18 @@
 package com.desafio.githubsearchjavapop.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.desafio.githubsearchjavapop.R;
+import com.desafio.githubsearchjavapop.model.Item;
 
 /**
  * Created by Mara on 07/12/2020
@@ -12,9 +20,42 @@ import com.desafio.githubsearchjavapop.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private TextView disconnected;
+    private Item item;
+    private ProgressDialog progressDialog;
+    private SwipeRefreshLayout swipeContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        swipeContainer = findViewById(R.id.swipeContainer);
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_orange_dark);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadJson();
+            }
+        });
+    }
+
+    private void loadJson() {
+        disconnected = findViewById(R.id.main_disconnected);
+    }
+
+    private void initViews() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Fetching Github users...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        recyclerView.smoothScrollToPosition(0);
+        loadJson();
     }
 }
