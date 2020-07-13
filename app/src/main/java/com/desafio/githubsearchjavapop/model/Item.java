2b613e3e6f58af26159962837e9882b7b2111e9c
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 08:11:38 -0300

    add repository items

--- app/src/main/java/com/desafio/githubsearchjavapop/model/Item.java
+++ app/src/main/java/com/desafio/githubsearchjavapop/model/Item.java
@@ -5,8 +5,97 @@ import com.google.gson.annotations.SerializedName;
 
 public class Item {
 
-    @SerializedName("")
+    @SerializedName("name")
+    @Expose
+    private String repoName;
+
+    @SerializedName("description")
+    @Expose
+    private String description;
+
     @SerializedName("login")
     @Expose
     private String login;
+
+    @SerializedName("avatar_url")
+    @Expose
+    private String avatarUrl;
+
+    @SerializedName("forks_count")
+    @Expose
+    private String forksCount;
+
+    @SerializedName("stargazers_count")
+    @Expose
+    private String stargazersCount;
+
+    @SerializedName("html_url")
+    @Expose
+    private String htmlUrl;
+
+    public Item(String repoName, String description, String login, String avatarUrl, String forksCount, String stargazersCount, String htmlUrl) {
+        this.repoName = repoName;
+        this.description = description;
+        this.login = login;
+        this.avatarUrl = avatarUrl;
+        this.forksCount = forksCount;
+        this.stargazersCount = stargazersCount;
+        this.htmlUrl = htmlUrl;
+    }
+
+    public String getRepoName() {
+        return repoName;
+    }
+
+    public void setRepoName(String repoName) {
+        this.repoName = repoName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getForksCount() {
+        return forksCount;
+    }
+
+    public void setForksCount(String forksCount) {
+        this.forksCount = forksCount;
+    }
+
+    public String getStargazersCount() {
+        return stargazersCount;
+    }
+
+    public void setStargazersCount(String stargazersCount) {
+        this.stargazersCount = stargazersCount;
+    }
+
+    public String getHtmlUrl() {
+        return htmlUrl;
+    }
+
+    public void setHtmlUrl(String htmlUrl) {
+        this.htmlUrl = htmlUrl;
+    }
 }
