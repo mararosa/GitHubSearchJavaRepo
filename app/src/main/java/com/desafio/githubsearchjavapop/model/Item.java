969c12250e93cf445965673e3d5583da50035564
Edimara Roberta Rosa <edimara.rosa@hotmail.com>
2020-07-13 07:47:04 -0300

    add a start to the layout

--- app/src/main/java/com/desafio/githubsearchjavapop/model/Item.java
+++ app/src/main/java/com/desafio/githubsearchjavapop/model/Item.java
@@ -1,4 +1,12 @@
 package com.desafio.githubsearchjavapop.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class Item {
+
+    @SerializedName("")
+    @SerializedName("login")
+    @Expose
+    private String login;
 }
